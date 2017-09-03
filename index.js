3af74544b5a33ec55de3196e3316748247eb2bb6
Miles Barr <milesbarr2@gmail.com>
2017-09-03 14:50:45 -0700

    Convert indentation to spaces

--- index.js
+++ index.js
@@ -1,131 +1,131 @@
 var bcrypt = require('bcrypt');
 
 module.exports = exports = function (schema, options) {
-	options = options || {};
-	options.saltWorkFactor = options.saltWorkFactor || 10;
-	options.maxAuthAttempts = options.maxAuthAttempts || 15;
-	options.accountLockTime = options.accountLockTime || (60 * 60);
-	options.accountLockTime *= 1000;
+  options = options || {};
+  options.saltWorkFactor = options.saltWorkFactor || 10;
+  options.maxAuthAttempts = options.maxAuthAttempts || 15;
+  options.accountLockTime = options.accountLockTime || (60 * 60);
+  options.accountLockTime *= 1000;
 
-	schema.add({
-		password: { type: String, required: true, maxLength: 255 },
-		authAttempts: { type: Number, required: true, default: 0 },
-		lockUntil: { type: Number }
-	});
+  schema.add({
+    password: { type: String, required: true, maxLength: 255 },
+    authAttempts: { type: Number, required: true, default: 0 },
+    lockUntil: { type: Number }
+  });
 
-	schema.virtual('isLocked').get(function () {
-		return this.lockUntil && this.lockUntil > Date.now();
-	});
+  schema.virtual('isLocked').get(function () {
+    return this.lockUntil && this.lockUntil > Date.now();
+  });
 
-	schema.pre('save', function (next) {
-		if (!this.isModified('password')) {
-			next();
-			return;
-		}
+  schema.pre('save', function (next) {
+    if (!this.isModified('password')) {
+      next();
+      return;
+    }
 
-		var user = this;
+    var user = this;
 
-		// Generate salt
-		bcrypt.genSalt(options.saltWorkFactor, function (err, salt) {
-			if (err) {
-				next(err);
-				return;
-			}
+    // Generate salt
+    bcrypt.genSalt(options.saltWorkFactor, function (err, salt) {
+      if (err) {
+        next(err);
+        return;
+      }
 
-			// Hash password
-			bcrypt.hash(user.password, salt, function (err, hashedPassword) {
-				if (err) {
-					next(err);
-					return;
-				}
+      // Hash password
+      bcrypt.hash(user.password, salt, function (err, hashedPassword) {
+        if (err) {
+          next(err);
+          return;
+        }
 
-				user.password = hashedPassword;
-				next();
-			});
-		});
-	});
+        user.password = hashedPassword;
+        next();
+      });
+    });
+  });
 
-	schema.method('comparePassword', function (candidatePassword, cb) {
-		bcrypt.compare(candidatePassword, this.password, cb);
-	});
+  schema.method('comparePassword', function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, cb);
+  });
 
-	schema.method('incAuthAttempts', function (cb) {
-		// Check if previous lock has expired
-		if (this.lockUntil && this.lockUntil < Date.now()) {
-			this.update({
-				$set: { authAttempts: 1 },
-				$unset: { lockUntil: 1 }
-			}, function (err, result) {
-				cb(err);
-			});
-			return;
-		}
+  schema.method('incAuthAttempts', function (cb) {
+    // Check if previous lock has expired
+    if (this.lockUntil && this.lockUntil < Date.now()) {
+      this.update({
+        $set: { authAttempts: 1 },
+        $unset: { lockUntil: 1 }
+      }, function (err, result) {
+        cb(err);
+      });
+      return;
+    }
 
-		// Increment attempts
-		var updates = { $inc: { authAttempts: 1 } };
+    // Increment attempts
+    var updates = { $inc: { authAttempts: 1 } };
 
-		// Lock account if reached max attempts
-		if (this.authAttempts + 1 >= options.maxAuthAttempts && !this.isLocked) {
-			updates.$set = { lockUntil: Date.now() + options.accountLockTime };
-		}
-		
-		this.update(updates, function (err, result) {
-			cb(err);
-		});
-	});
+    // Lock account if reached max attempts
+    if (this.authAttempts + 1 >= options.maxAuthAttempts && !this.isLocked) {
+      updates.$set = { lockUntil: Date.now() + options.accountLockTime };
+    }
+    
+    this.update(updates, function (err, result) {
+      cb(err);
+    });
+  });
 
-	schema.static('auth', function (conditions, password, cb) {
-		this.findOne(conditions, function (err, user) {
-			if (err || !user) {
-				cb(err);
-				return;
-			}
+  schema.static('auth', function (conditions, password, cb) {
+    this.findOne(conditions, function (err, user) {
+      if (err || !user) {
+        cb(err);
+        return;
+      }
 
-			// Check if account is currently locked
-			if (user.isLocked) {
-				user.incAuthAttempts(function (err) {
-					if (err) {
-						cb(err);
-						return;
-					}
-					cb(null, user);
-				});
-				return;
-			}
+      // Check if account is currently locked
+      if (user.isLocked) {
+        user.incAuthAttempts(function (err) {
+          if (err) {
+            cb(err);
+            return;
+          }
+          cb(null, user);
+        });
+        return;
+      }
 
-			// Check password
-			user.comparePassword(password, function (err, isMatch) {
-				if (err) {
-					cb(err);
-					return;
-				}
+      // Check password
+      user.comparePassword(password, function (err, isMatch) {
+        if (err) {
+          cb(err);
+          return;
+        }
 
-				// Was the password a match?
-				if (!isMatch) {
-					user.incAuthAttempts(function (err) {
-						cb(err);
-					});
-					return;
-				}
-				
-				// Return if the user has no failed attempts and is not locked
-				if (!user.authAttempts && !user.lockUntil) {
-					cb(null, user);
-					return;
-				}
+        // Was the password a match?
+        if (!isMatch) {
+          user.incAuthAttempts(function (err) {
+            cb(err);
+          });
+          return;
+        }
+        
+        // Return if the user has no failed attempts and is not locked
+        if (!user.authAttempts && !user.lockUntil) {
+          cb(null, user);
+          return;
+        }
 
-				// Reset attempts
-				user.update({
-					$set: { authAttempts: 0 },
-					$unset: { lockUntil: 1 }
-				}, function (err) {
-					if (err) {
-						cb(err);
-						return;
-					}
-					cb(null, user);
-				});
-			});
-		});
-	});
+        // Reset attempts
+        user.update({
+          $set: { authAttempts: 0 },
+          $unset: { lockUntil: 1 }
+        }, function (err) {
+          if (err) {
+            cb(err);
+            return;
+          }
+          cb(null, user);
+        });
+      });
+    });
+  });
 };
